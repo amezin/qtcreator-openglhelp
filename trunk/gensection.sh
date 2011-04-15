8ddab2cada169e9e23d7ac8a7b1d6b4b7e6bf9cb
egon.rath@gmail.com <egon.rath@gmail.com@c155de30-3a77-f74b-d18e-222413280ea2>
2011-04-15 08:35:59 +0000

    fixed some bugs; added support for opengl 3.3 and 4.1

--- trunk/gensection.sh
+++ trunk/gensection.sh
@@ -4,7 +4,7 @@ PREFIX=${2}
 cat ${INDEX} | grep "href=\"gl" | while read line
 do
     KEY=`echo $line | awk -F '>' '{print $4}' | awk -F '<' '{print $1}'`
-    VAL=`echo $line | awk -F '"' '{print $4}'`
+    VAL=`echo $line | awk -F '"' '{print $4}' | sed s/xml/html/g`
    
    echo "<section title=\"${KEY}\" ref=\"${PREFIX}/${VAL}\"/>" 
 done
